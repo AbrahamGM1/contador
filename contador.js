6ba777606c2b53a9c8a5891670a976f3be69e10d
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2023-12-10 13:54:51 -0700

    mecanismo base de colas para las etiquetas

--- contador.js
+++ contador.js
@@ -12,13 +12,34 @@ var arregloEtiquetas = []
 var primeraConsulta = true
 
 
+//Clase de las colas para procesar las etiquetas
+class Queue{
+
+  #items = [];
+
+  enqueue(item){
+    this.#items.push(item)
+  }
+
+  dequeue(){
+    return this.#items.shift()
+  }
+
+  isEmpty(){
+    return this.#items.length === 0;
+  }
+
+}
+
+const queue = new Queue();
+
+
 window.onload = function () {
   //document.querySelector('button').addEventListener('click', function() {
   chrome.identity.getAuthToken({ interactive: true }, function (token) {
     chrome.runtime.sendMessage({ action: 'authToken', tokenGuardar: token });
   });
 
-  iniciarContador()
 
 };
 
@@ -100,15 +121,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //Cambia entre verdadero o falso segun la situcación en la que se encuentre la variable de inicio
   boton.addEventListener("click", function () {
-    presionarGrabacion()
+    presionarGrabacion(boton)
   });
 
-  /**
-   * La siguiente funcion se encarga de obtener lo que esté escrito dentro del 
-   * campo de texto para despues irlo desplegando en una lista, a su vez que va
-   * guardando cada etiqueta dentro del localstorage. Por ahora nomás hará agregar,
-   * mas adelante se debera de poder modificar o eliminar.
-   */
+
+  //Para guardar las etiquetas
   botonOk.addEventListener("click", function () {
     let nuevaEtiqueta = txtEtiqueta.value
     //Si no hay nada ps no hace nada
@@ -117,33 +134,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     //Si hay algo escrito, se guarda dentro del local storage y lo despliega debajo
     else if (nuevaEtiqueta != "") {
-      let cadenacompleta = txtEtiqueta.value + ":" + formatearMS(acumulado)
-      arregloEtiquetas.push(cadenacompleta)
-
-      ///Mete dentro del arreglo el valor de la cadena
-      //Mete el arreglo de puras strings al localstorage
-      //Itera en un foreach todas las strings del arreglo
-      //y muestralas en forma de lista
-      localStorage.setItem('arregloEtiquetas', JSON.stringify(arregloEtiquetas))
-
-      let arregloFinal = JSON.parse(localStorage.getItem('arregloEtiquetas'))
-
-      if (primeraConsulta) {
-        const parrafos = document.querySelectorAll("p")
-        parrafos.forEach(function (parrafo) {
-          parrafo.remove();
-        });
-      }
-
-
-      arregloFinal.forEach(element => {
-        let parrafo = document.createElement('p')
-        parrafo.className = "parrafo"
-        parrafo.innerHTML = element
-        document.body.appendChild(parrafo)
-      });
-
-
+      guardarEtiqueta(nuevaEtiqueta)
     }
 
   });
@@ -151,13 +142,60 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-function presionarGrabacion() {
+/**
+ * La siguiente funcion se encarga de obtener lo que esté escrito dentro del 
+ * campo de texto para despues irlo desplegando en una lista, a su vez que va
+ * guardando cada etiqueta dentro del localstorage. Por ahora nomás hará agregar,
+ * mas adelante se debera de poder modificar o eliminar.
+ */
+function guardarEtiqueta(txtEtiqueta) {
+  let cadenacompleta = txtEtiqueta + ":" + formatearMS(acumulado)
+  arregloEtiquetas.push(cadenacompleta)
+
+  ///Mete dentro del arreglo el valor de la cadena
+  //Mete el arreglo de puras strings al localstorage
+  //Itera en un foreach todas las strings del arreglo
+  //y muestralas en forma de lista
+  localStorage.setItem('arregloEtiquetas', JSON.stringify(arregloEtiquetas))
+
+  let arregloFinal = JSON.parse(localStorage.getItem('arregloEtiquetas'))
+
+  if (primeraConsulta) {
+    const parrafos = document.querySelectorAll("p")
+    parrafos.forEach(function (parrafo) {
+      parrafo.remove();
+    });
+  }
+
+
+  arregloFinal.forEach(element => {
+    let parrafo = document.createElement('p')
+    parrafo.className = "parrafo"
+    parrafo.innerHTML = element
+    document.body.appendChild(parrafo)
+  });
+
+  queue.enqueue(procesarEtiqueta(3000, cadenacompleta))
+
+  while (!queue.isEmpty()) {
+    const fn = queue.dequeue();
+    fn.then(data => {
+      // Conectar con el web service usando los datos
+      alert(data);
+    });
+  }
+
+}
+
+function presionarGrabacion(boton) {
   //Si se está "grabando" y se pulsa el botón, se detiene la "grabación" y se pone en 0 todo
   if (inicio == true) {
     inicio = false;
     localStorage.setItem('contador', JSON.stringify({ inicioGuardado: 0, acumuladoGuardado: 0, tiempoRefGuardado: 0, activo: inicio }));
     acumulado = 0;
     boton.innerHTML = "Grabar sesión";
+
+
   } else if (inicio == false) {
     //Si esta detenido, pues "inicia la grabación"
     inicio = true;
@@ -169,4 +207,12 @@ function iniciarContador() {
   //Si esta detenido, pues "inicia la grabación"
   inicio = true;
   boton.innerHTML = "Detener grabación";
+}
+
+function procesarEtiqueta(tiempo, etiqueta) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(etiqueta); // Resuelve la promesa con la etiqueta
+    }, tiempo);
+  });
 }
